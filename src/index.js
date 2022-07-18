0d7b536823792b2610b5f59d5c0eefee409b7145
Avichez <starkavichi@gmail.com>
2022-07-18 15:42:10 +0300

    feat: Task brackets completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const stack = [];
+    const objectsArray = bracketsConfig.map((el) => ({ [el[0]]: el[1] }));
+    const hashMap = Object.assign({}, ...objectsArray);
+
+    for (let symb of str) {
+        if (hashMap[symb] && !stack.includes(symb)) {
+            stack.push(hashMap[symb]);
+        } else if (stack.length > 0 && stack[stack.length - 1] === symb) {
+            stack.pop();
+        } else {
+            return false;
+        }
+    }
+    return stack.length === 0;
+};
